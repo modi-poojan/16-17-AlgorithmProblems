85cc843ec1906fdee2b86710978c281057134d39
modi-poojan <80687912+modi-poojan@users.noreply.github.com>
2021-12-10 21:21:36 +0530

    Ability to find your number using binary search

--- Algorithm_Main.java
+++ Algorithm_Main.java
@@ -1,13 +1,18 @@
 package com.Algorithms;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Algorithm_Main {
 
 	public static void main(String[] args) {
-		
-		PrimeAnagramPalindrome call = new PrimeAnagramPalindrome();
-		call.primeAnagram();
-}
+		@SuppressWarnings("resource")
+		Scanner input = new Scanner(System.in);
+		FindNumber call = new FindNumber();
+
+		System.out.println("\n Enter the range of number for program\n");
+		int range = input.nextInt();
+
+		System.out.println("Think of any number between 0-" + range + "\n");
+		call.searchNumber(0, range);
+	}
 }
